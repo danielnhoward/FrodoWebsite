8f65525e627c41a4e9f2dc44c8965ded152c8002
Toffee1347 <danielnhoward93@gmail.com>
2021-05-23 20:49:32 +0100

    changed capitals

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import FirstSection from "./Home/sectionOne.js";
-import Section from "./Home/section.js";
-
+import Section from "./Home/Section.js";
 
 export default function Home() {
     return (
