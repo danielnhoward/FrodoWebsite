6f722940e6b5cdf10e1548325e4ba7e890982eb2
Toffee1347 <danielnhoward93@gmail.com>
2021-05-24 19:30:10 +0100

    change images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import FirstSection from "./Home/sectionOne.js";
 import Section from "./Home/Section.js";
+// import { Card, CardActionArea, CardContent } from "@material-ui/core";
 
 export default function Home() {
     return (
@@ -9,16 +10,24 @@ export default function Home() {
             </Section>
             <Section>
                 <div>
-                    <b style={{fontSize: 'x-large'}}>Who is Frodo?</b>
-                    <br/>
-                    <p>
-                        Frodo is a minigames discord bot that is constantly being developed. It has many fun and polished games and more are coming. Frodo features many games like tic tac toe, connect 4 and trivia!
-                    </p>
+                    {/* <Card>
+                        <CardContent> */}
+                            <b style={{fontSize: 'x-large'}}>Who is Frodo?</b>
+                            <br/>
+                            <p>
+                                Frodo is a minigames discord bot that is constantly being developed. It has many fun and polished games and more are coming. Frodo features many games like tic tac toe, connect 4 and trivia!
+                            </p>
+                        {/* </CardContent>
+                    </Card> */}
                     <br/><br/>
-                    <b style={{fontSize: 'x-large'}}>Why Frodo?</b>
-                    <p>
-                        Frodo has all the games in one simple place, no need to move between different bots, just check out all the wide range of commands that we offer and take your pick!
-                    </p>
+                    {/* <Card>
+                        <CardContent> */}
+                            <b style={{fontSize: 'x-large'}}>Why Frodo?</b>
+                            <p>
+                                Frodo has all the games in one simple place, no need to move between different bots, just check out all the wide range of commands that we offer and take your pick!
+                            </p>
+                        {/* </CardContent>
+                    </Card> */}
                 </div>
                 <div>
                     <img alt="Frodo preview" width="400" src="/static/img/frodoProfile.svg" className="frodoPreview"/>
